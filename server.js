219b1410d589e5325ca446d8cfc12e560ed2ade2
panwaratul <panwaratul82@gmail.com>
2017-04-24 20:47:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,112 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles= {
+'article-one': {
+  title: 'Article One | Atul Panwar',
+  heading: 'Article One',
+  date: 'Sep 5,2016',
+  content: `<p>
+                This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+            </p>
+            
+            <p>
+                This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+            </p>
+            <p>
+                This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+            </p>`
+  
+},
+'article-two': {
+    title: 'Article Two | Atul Panwar',
+  heading: 'Article Two',
+  date: 'Sep 6,2016',
+  content: `<p>
+                This is the content of my second article.
+            </p>`
+            
+         },
+'article-three': {
+    title: 'Article Three | Atul Panwar',
+  heading: 'Article Three',
+  date: 'Sep 7,2016',
+  content: `<p>
+                This is the content of my third article.
+            </p>`
+            
+           
+}
+};
+function createTemplate(data){
+var title= data.title;
+var date= data.date;
+var heading= data.heading;
+var content= data.content;
+
+var htmlTemplate=`
+<html>
+<head>
+    <title>
+     ${title}
+        </title>
+        <meta name="viewport" content="width=device-width , initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+         <div>
+         <a href="/">Home</a>
+         </div>
+         <hr/>
+         <h3>
+            ${heading}
+         </h3>
+        
+         <div>
+             ${date}
+         </div>
+         <div>
+           ${content}
+         </div>
+    </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res){
+counter=counter + 1;
+res.send(counter.toString());
+
+});
+
+var names=[];
+app.get('/submit-name', function (req, res){ //URL:/submit-name?name=XXXXX
+    //Get the name from request object
+    var name = req.query.name;
+    names.push(name);
+    //JSON javascript object notation
+    res.send(JSON.stringify(names));
+});
+
+app.get('/:articleName',function (req, res){
+    var articleName = req.params.articleName;
+ res.send(createTemplate(articles[articleName]));  
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
